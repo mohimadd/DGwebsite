57f7a9f1cae12fe0302e1ee7f950be72ac2845a6
mohimadd <m.imadudin68@gmail.com>
2024-12-28 00:05:25 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,79 @@
+import streamlit as st
+
+rak1,rak2,rak3,rak4 = st.columns(4)
+
+with rak1:
+    st.image('digmart.jpeg',width=400)
+    st.markdown(
+        '<h1 style="font-size:32px;">Ini Judul Buku</>', unsafe_allow_html=True
+    )
+    st.markdown('<h2 style="font-size:19px;">Ini Harga Buku</>', unsafe_allow_html=True
+    )
+    st.link_button('ini tombol link','https://www.google.com')
+
+with rak2:
+    st.image('digmart.jpeg',width=400)
+    st.markdown(
+        '<h1 style="font-size:32px;">Ini Judul Buku</>', unsafe_allow_html=True
+    )
+    st.markdown('<h2 style="font-size:19px;">Ini Harga Buku</>', unsafe_allow_html=True
+    )
+    st.link_button('ini tombol link','https://www.google.com')
+
+with rak3:
+    st.image('digmart.jpeg',width=400)
+    st.markdown(
+        '<h1 style="font-size:32px;">Ini Judul Buku</>', unsafe_allow_html=True
+    )
+    st.markdown('<h2 style="font-size:19px;">Ini Harga Buku</>', unsafe_allow_html=True
+    )
+    st.link_button('ini tombol link','https://www.google.com')
+
+with rak4:
+    st.image('digmart.jpeg',width=400)
+    st.markdown(
+        '<h1 style="font-size:32px;">Ini Judul Buku</>', unsafe_allow_html=True
+    )
+    st.markdown('<h2 style="font-size:19px;">Ini Harga Buku</>', unsafe_allow_html=True
+    )
+    st.link_button('ini tombol link','https://www.google.com')
+
+
+
+rak5,rak6,rak7,rak8 = st.columns(4)
+
+with rak5:
+    st.image('digmart.jpeg',width=400)
+    st.markdown(
+        '<h1 style="font-size:32px;">Ini Judul Buku</>', unsafe_allow_html=True
+    )
+    st.markdown('<h2 style="font-size:19px;">Ini Harga Buku</>', unsafe_allow_html=True
+    )
+    st.link_button('ini tombol link','https://www.google.com')
+
+with rak6:
+    st.image('digmart.jpeg',width=400)
+    st.markdown(
+        '<h1 style="font-size:32px;">Ini Judul Buku</>', unsafe_allow_html=True
+    )
+    st.markdown('<h2 style="font-size:19px;">Ini Harga Buku</>', unsafe_allow_html=True
+    )
+    st.link_button('ini tombol link','https://www.google.com')
+
+with rak7:
+    st.image('digmart.jpeg',width=400)
+    st.markdown(
+        '<h1 style="font-size:32px;">Ini Judul Buku</>', unsafe_allow_html=True
+    )
+    st.markdown('<h2 style="font-size:19px;">Ini Harga Buku</>', unsafe_allow_html=True
+    )
+    st.link_button('ini tombol link','https://www.google.com')
+
+with rak8:
+    st.image('digmart.jpeg',width=400)
+    st.markdown(
+        '<h1 style="font-size:32px;">Ini Judul Buku</>', unsafe_allow_html=True
+    )
+    st.markdown('<h2 style="font-size:19px;">Ini Harga Buku</>', unsafe_allow_html=True
+    )
+    st.link_button('ini tombol link','https://www.google.com')
